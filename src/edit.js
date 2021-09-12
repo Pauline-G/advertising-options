c872e0e27fc072868bc93cdc8f4771d4acdd8fdb
Pauline-G <pauline@intermedia.com.au>
2021-09-12 21:06:38 +1000

    Add files via upload

---
+++ src/edit.js
@@ -0,0 +1,15 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+import { TextControl } from '@wordpress/components';
+import './editor.scss';
+ 
+export default function Edit( { attributes, setAttributes } ) {
+    return (
+        <TextControl
+            { ...useBlockProps() }
+            value={ attributes.message }
+            onChange={ ( val ) => setAttributes( { message: val } ) }
+        />
+    );
+    
+}
